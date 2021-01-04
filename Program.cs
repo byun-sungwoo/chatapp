9bf005fd501d8ad5a40a7275be4f652ce398b8cd
byun-sungwoo <dbyun1999@gmail.com>
2021-01-04 02:56:54 -0500

    web app is deployable

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
+//using Azure.Identity;
 
 namespace ChatApp
 {
@@ -19,20 +19,20 @@ namespace ChatApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
+//.ConfigureAppConfiguration((context, config) =>
+//{
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//config.AddAzureKeyVault(
+//keyVaultEndpoint,
+//new DefaultAzureCredential());
+//})
+//.ConfigureAppConfiguration((context, config) =>
+//{
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//config.AddAzureKeyVault(
+//keyVaultEndpoint,
+//new DefaultAzureCredential());
+//})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
